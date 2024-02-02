202a927f2074ecadb011c0b782f51e12f34e6a30
Kumar Parikshit <87370837+KumarParikshit@users.noreply.github.com>
2024-02-03 01:19:25 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -110,7 +110,7 @@ const UpiForm = () => {
 
   }
 
-  return (
+    {/*return (
     <>
       <SeoMeta />
       <HeroSection />
@@ -235,7 +235,7 @@ const UpiForm = () => {
                     type="submit"
                     onClick={redirectToPage}
                     className={`${inter.className} w-full mt-6 flex justify-center py-4 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
-                  >
+                  >*/}
                     Preview
                   </button>
                 </div>
