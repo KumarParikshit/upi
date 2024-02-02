38fdac3b2f79eee8f7b387606b6e22ddd0d751fd
Kumar Parikshit <87370837+KumarParikshit@users.noreply.github.com>
2024-02-03 01:11:30 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,9 +16,9 @@ export const HeroSection = () => {
         <div className="relative z-10 max-w-screen-xl mx-auto px-4 py-28 md:px-8">
           <div className="space-y-5 max-w-4xl mx-auto text-center">
             <h1 className={`${poppins.className} text-4xl text-white font-extrabold mx-auto md:text-5xl`}>
-              Generate upi link for free
+              PAYMENT GATEWAY MADE FOR AND BY CARDERS ZONE TELEGRAM GROUP
             </h1>
-            <p className={`${inter.className} max-w-2xl mx-auto text-gray-400`}>Scan QR code or directly open with any upi app</p>
+            <p className={`${inter.className} max-w-2xl mx-auto text-gray-400`}>NOTE :- THIS IS THE V1 VERSION OF THE GATEWAY V2 VERSION IS UNDER DEVELOPMENT AND WHEN IT WILL COME WITH LOT OF FEATURES JUST LIKE RAZORPAY ITS MY PROMISE THAY IT WILL COME IN 5-10 DAYS. </p>
           </div>
         </div>
         <div
